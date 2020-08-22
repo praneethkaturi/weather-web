a6c9031166628b4702d1081c4822af24d2570352
praneethkaturi <praneeth.katuri007@gmail.com>
2020-08-22 10:30:54 +0530

    testing first commit

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,6 @@ const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg1')
 const msg2 = document.querySelector('#msg2')
 
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
